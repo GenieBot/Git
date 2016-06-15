8601ef275c325d42fd92d10d14628dc509675988
SpongyBacon <spongybacon@hotmail.co.uk>
2016-06-15 08:04:56 +0100

    First commit

---
+++ src/main/java/io/sponges/bot/modules/git/BotGit.java
@@ -0,0 +1,20 @@
+package io.sponges.bot.modules.git;
+
+import io.sponges.bot.api.module.Module;
+
+public class BotGit extends Module {
+
+    public BotGit() {
+        super("Git", "1.0");
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
